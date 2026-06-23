f1d22e996e9f78e34a76b57c2c35ecc9c82bd389
codesam48 <ssamwokos48@gmail.com>
2026-06-23 08:13:57 -0700

    Im done with HTMl

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { FaCalendar } from "react-icons/fa"
+import EventList from "./Components/EventList"
+import AddEvents from "./Components/AddEvents"
 function App() {
   return (
-    <section>
-      <div className="flex">
+    <section className="">
+      <div className="flex justify-between">
         <span>
           <FaCalendar />
         </span>
-        <h1>Event Tracker</h1>
+        <h1><span>Countdown</span>  Event Tracker</h1>
+        <div>
+          <span>
+            <FaCalendar />
+          </span>
+          <div className="text-sm">
+            <p>Today is</p>
+            <p>
+              <span>june</span> /<span>28</span> /<span>2025</span>
+            </p>
+          </div>
+        </div>
 
       </div>
+      {/* add event section */}
+      <AddEvents/>
+      <EventList />
     </section>
   )
 }
