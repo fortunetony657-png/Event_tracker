0eff3b4e7cb9a124dfd335b43d19ebe922f694dd
fortunetony657-png <fortunetony657@gmail.com>
2026-06-23 13:58:56 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { FaCalendar } from "react-icons/fa"
+function App() {
+  return (
+    <section>
+      <div className="flex">
+        <span>
+          <FaCalendar />
+        </span>
+        <h1>Event Tracker</h1>
+
+      </div>
+    </section>
+  )
+}
+
+export default App
